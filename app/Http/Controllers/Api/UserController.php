fafc7cbc15bb649bae312717525dfdd2259ee739
maram <mramadan@benaagroup.com.sa>
2019-07-15 17:16:04 +0200

    output handled

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\UpdateUserRequest;
 use App\Http\Requests\UserRequest;
-use App\Transformers\UserTransformer;
 use Illuminate\Http\Request;
 use App\User;
 use Illuminate\Support\Facades\Validator;
@@ -14,6 +13,9 @@ use Carbon\Carbon;
 use Spatie\Permission\Models\Role;
 use DB;
 use Hash;
+use League\Fractal;
+use League\Fractal\Manager;
+use League\Fractal\Resource\Collection;
 
 class UserController extends Controller
 {
@@ -27,15 +29,25 @@ class UserController extends Controller
     public function show(User $user)
     {
         $this->authorize('index', User::class);
-//        return response()->json(['user' => $user], 200);
-        return fractal($user, new UserTransformer());
+        $output = [
+            'status' => 200,
+            'message' => 'User loaded successfully',
+            'data' => $user
+        ];
+        return response()->json($output);
     }
 
     public function index()
     {
         $this->authorize('index', User::class);
-//        return response()->json(['users' =>$data ], 200);
-        return fractal(User::orderBy('id','DESC')->get(), new UserTransformer());
+        $data = User::orderBy('id','DESC')->get();
+        $output = [
+            'status' => 200,
+            'message' => 'User loaded successfully',
+            'data' => $data
+        ];
+        return response()->json($output);
+
     }
 
     /**
@@ -48,7 +60,14 @@ class UserController extends Controller
     {
         $this->authorize('index', User::class);
         $roles = Role::pluck('name')->all();
-        return response()->json(['roles' =>$roles ], 200);
+        $output = [
+            'status' => 200,
+            'message' => 'roles loaded successfully',
+            'data' => [
+                'roles' => $roles
+            ]
+        ];
+        return response()->json($output);
     }
 
 
@@ -63,21 +82,14 @@ class UserController extends Controller
         $this->authorize('index', User::class);
         $data = $request->validated();
 
-        if ($request->hasFile('image')) {
-            $image = $request->file('image');
-
-            $file_name = md5(time() . $image->getClientOriginalName()) . '.' . $image->getClientOriginalExtension();
-            $destinationPath = public_path() . '/uploads/images/user/';
-            $image->move($destinationPath, $file_name);
-
-            $data['image'] = $file_name;
-        }
-
         $data['password'] = bcrypt($data['password']); //Hash password
-
         $user = User::create($data);
         $user->assignRole($request->input('roles'));
-        return response()->json(['success' =>'User created successfully' ], 200);
+        $output = [
+            'status' => 200,
+            'message' => 'User created successfully',
+        ];
+        return response()->json($output);
     }
 
 
@@ -96,10 +108,15 @@ class UserController extends Controller
         $this->authorize('edit', [User::class, $user]);
         $roles = Role::pluck('name')->all();
         $userRole = $user->roles->pluck('name')->first();
-        return response()->json(['user' =>$user ,
-            'roles' => $roles,
-            'userRole' => $userRole
-        ], 200);
+        $output = [
+            'status' => 200,
+            'message' => 'Roles loaded successfully',
+            'data' => [
+                'roles' => $roles,
+                'userRole' => $userRole
+            ]
+        ];
+        return response()->json($output);
     }
 
     /**
@@ -111,29 +128,44 @@ class UserController extends Controller
      * @throws \Illuminate\Auth\Access\AuthorizationException
      */
 
-    public function update(UpdateUserRequest $request, User $user)
+    public function update(Request $request, User $user)
     {
         $this->authorize('edit', [User::class, $user]);
 
-        $data = $request->validated();
+        $data = $request->all();
 
-        if(!empty($data['password'])){
-            $data['password'] = bcrypt($data['password']); //update the password
+        DB::beginTransaction();
+        try{
+
+            if(!empty($input['password'])){
+                $input['password'] = Hash::make($input['password']); //update the password
+            }
+            if ($request->hasFile('image')) {
+                $image = $request->file('image');
+
+                $file_name = md5(time() . $image->getClientOriginalName()) . '.' . $image->getClientOriginalExtension();
+                $destinationPath = public_path() . '/uploads/images/user/';
+                $image->move($destinationPath, $file_name);
+
+                $data['image'] = $file_name;
+            }
+            $user->update($data);
+            $user->save();
+            DB::table('model_has_roles')->where('model_id',$user->id)->delete();
+            $user->assignRole($request->input('roles'));
+            DB::commit();
+            $output = [
+                'status' => 200,
+                'message' => 'User updated successfully',
+            ];
+        } catch (\Exception $e) {
+            DB::rollback();
+            $output = [
+                'status' => 500,
+                'error' => $e->getMessage(),
+            ];
         }
-
-        if ($request->hasFile('image')) {
-            $image = $request->file('image');
-
-            $file_name = md5(time() . $image->getClientOriginalName()) . '.' . $image->getClientOriginalExtension();
-            $destinationPath = public_path() . '/uploads/images/user/';
-            $image->move($destinationPath, $file_name);
-
-            $data['image'] = $file_name;
-        }
-
-        $user->update($data);
-        $user->save();
-        return response()->json(['success' =>'User updated successfully','User' => $user ], 200);
+        return response()->json($output);
 
     }
 
@@ -149,6 +181,10 @@ class UserController extends Controller
     {
         $this->authorize('edit', [User::class, $user]);
         $user->delete();
-        return response()->json(['success' =>'User deleted successfully'], 200);
+        $output = [
+            'status' => 200,
+            'message' => 'User deleted successfully',
+        ];
+        return response()->json($output);
     }
 }
